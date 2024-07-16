7356a4bf11d6d7d8cba6d5efd6e729c5b3a3c037
Rafael Francisco <broncasrafa@gmail.com>
2024-07-16 15:44:24 -0300

    [DEPLOY] servidor em node.js

--- server.js
+++ server.js
@@ -14,15 +14,38 @@ const port = process.env.PORT || 3005;
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 
+
 // Configurações do CORS
+const allowedOrigins = [
+  'https://ambitious-water-0664a3d10.5.azurestaticapps.net',
+  'http://localhost:4200'
+];
 const corsOptions = {
-    origin: 'https://ambitious-water-0664a3d10.5.azurestaticapps.net', // URL do seu frontend Angular
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  };
+  origin: function (origin, callback) {
+    console.log('Origin:', origin);  // Adicione este log
+      // Permitir solicitações sem origem (ex: mobile apps ou CURL)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+          const msg = 'The CORS policy for this site does not allow access from the specified Origin X.';
+          return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+// Configurações do CORS
+// const corsOptions = {
+//     origin: 'https://ambitious-water-0664a3d10.5.azurestaticapps.net', // URL do seu frontend Angular
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
 
 // for logs
 app.use((req, res, next) => {
